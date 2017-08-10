f5d1f736c6ba9416a9f1157b08baf00e5571292a
AkshayNawale <akshay.nawale123@hotmail.com>
2017-08-10 14:37:09 +0530

    re-commiting

--- GitHubEclipse/src/initPackage/MyJavaCode.java
+++ GitHubEclipse/src/initPackage/MyJavaCode.java
@@ -4,9 +4,8 @@ public class MyJavaCode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] str = {"akshay1, akshay2, akshay3, akshay4, akshay5"};
-		//display names - changes
-		//added code directly from Github
+		String[] str = {"akshay2, akshay3, akshay4, akshay5"};
+		//display names
 		for(String str1: str)
 		{
 			System.out.println(str1);
