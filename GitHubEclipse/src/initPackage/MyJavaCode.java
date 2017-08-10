20d06f32e6d67a41adb47961b29c245aa990e581
AkshayNawale <akshay.nawale123@hotmail.com>
2017-08-10 14:51:37 +0530

    Commit to Feature 1

--- GitHubEclipse/src/initPackage/MyJavaCode.java
+++ GitHubEclipse/src/initPackage/MyJavaCode.java
@@ -1,12 +1,11 @@
 package initPackage;
-
+// Final Code
 public class MyJavaCode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String[] str = {"akshay1, akshay2, akshay3, akshay4, akshay5"};
 		//display names
-		//changes from GitHub from Master
 		for(String str1: str)
 		{
 			System.out.println(str1);
