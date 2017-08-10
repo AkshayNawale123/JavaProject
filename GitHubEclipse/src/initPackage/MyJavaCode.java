933ba6466bbc147d3e2ef0abd534e3ef368fd964
Akshay <30859158+AkshayNawale123@users.noreply.github.com>
2017-08-10 14:30:19 +0530

    Added code directly from Github
    updated name and comment

--- GitHubEclipse/src/initPackage/MyJavaCode.java
+++ GitHubEclipse/src/initPackage/MyJavaCode.java
@@ -4,8 +4,9 @@ public class MyJavaCode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] str = {"akshay1, akshay2, akshay3, akshay4"};
-		//display names - changes 
+		String[] str = {"akshay1, akshay2, akshay3, akshay4, akshay5"};
+		//display names - changes
+		//added code directly from Github
 		for(String str1: str)
 		{
 			System.out.println(str1);
