68cf256768668d80c727735b99567a0c3e88aa56
AkshayNawale <akshay.nawale123@hotmail.com>
2017-08-10 14:47:54 +0530

    Comment

--- GitHubEclipse/src/initPackage/MyJavaCode.java
+++ GitHubEclipse/src/initPackage/MyJavaCode.java
@@ -5,7 +5,7 @@ public class MyJavaCode {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String[] str = {"akshay1, akshay2, akshay3, akshay4, akshay5"};
-		//display names, deleting comments from feature1		
+		//display names		
 		for(String str1: str)
 		{
 			System.out.println(str1);
