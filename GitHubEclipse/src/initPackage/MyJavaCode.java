843ac346b581f07e75ea1547a388081da8344721
AkshayNawale <akshay.nawale123@hotmail.com>
2017-08-10 14:23:19 +0530

    Adding Project to GitHub

---
+++ GitHubEclipse/src/initPackage/MyJavaCode.java
@@ -0,0 +1,15 @@
+package initPackage;
+
+public class MyJavaCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] str = {"akshay1, akshay2, akshay3"};
+		//display names
+		for(String str1: str)
+		{
+			System.out.println(str1);
+		}
+	}
+
+}
