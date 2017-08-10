87032f56b499a7f788f8be2233bc98710d32a76c
Akshay <30859158+AkshayNawale123@users.noreply.github.com>
2017-08-10 15:46:12 +0530

    Direct Append

--- GitHubEclipse/src/initPackage/MyJavaCode.java
+++ GitHubEclipse/src/initPackage/MyJavaCode.java
@@ -5,11 +5,10 @@ public class MyJavaCode {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String[] str = {"akshay1, akshay2, akshay3, akshay4, akshay5"};
-		//display names on new line.
+		
 		for(String str1: str)
 		{
 			System.out.println(str1);
-			System.out.println();
 		}
 	}
 }
