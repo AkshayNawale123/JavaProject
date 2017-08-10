b4151200d2ceaccd571163efd012576e712c2bd7
AkshayNawale <akshay.nawale123@hotmail.com>
2017-08-10 14:26:37 +0530

    Added new names and comment

--- GitHubEclipse/src/initPackage/MyJavaCode.java
+++ GitHubEclipse/src/initPackage/MyJavaCode.java
@@ -4,8 +4,8 @@ public class MyJavaCode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] str = {"akshay1, akshay2, akshay3"};
-		//display names
+		String[] str = {"akshay1, akshay2, akshay3, akshay4"};
+		//display names - changes 
 		for(String str1: str)
 		{
 			System.out.println(str1);
